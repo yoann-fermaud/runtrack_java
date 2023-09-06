695a1d83f66e249fa098091a05979611f48ad6e3
yoann-fermaud <yoann.fermaud@laplateforme.io>
2023-09-06 14:14:32 +0200

    [ADD] runtrack day01

---
+++ src/jour1/job01/HelloWorld.java
@@ -0,0 +1,14 @@
+package jour1.job01;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
